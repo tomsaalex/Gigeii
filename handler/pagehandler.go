44ad7309143a07acebe80d9da69d2e413eb8aa6f
aletaslauan <aletaslauan@gmail.com>
2025-06-10 02:14:35 +0300

    modify page handler

--- handler/pagehandler.go
+++ handler/pagehandler.go
@@ -1,11 +1,11 @@
 package handler
 
 import (
-    "context"
-    "net/http"
+	"context"
+	"net/http"
 
-    "example.com/templates/base"
-    "example.com/templates/pages"
+	"example.com/templates/base"
+	"example.com/templates/pages"
 )
 
 type PageHandler struct{}
@@ -21,19 +21,5 @@ func (h *PageHandler) homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *PageHandler) fullCalendarPage(w http.ResponseWriter, r *http.Request) {
-    base.PageSkeleton(pages.FullCalendarPage()).Render(context.Background(), w)
+	base.PageSkeleton(pages.FullCalendarPage()).Render(context.Background(), w)
 }
-
-/*
-func (h *AvailabilityHandler) getAvailabilities(w http.ResponseWriter, r *http.Request) {
-    availabilities, err := h.availabilityService.GetAllAvailabilities(r.Context())
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        custalerts.MakeAlertDanger("Failed to fetch availabilities.").Render(r.Context(), w)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(availabilities)
-}
-
-*/
